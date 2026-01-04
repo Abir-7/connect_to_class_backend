f650ff5104e3d83222f09b04a2710a254c74c199
Abir-7 <md.tazwarul.islam.07@gmail.com>
2026-01-04 11:38:37 +0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ import { StripeController } from "./app/modules/stripe/stripe.controller";
 const app = express();
 
 const allowedOrigins = [
-  "http://10.10.12.62:3000",
+  "http://10.10.12.63:3000",
   "http://192.168.50.85:3000",
   "http://72.62.7.53:3000",
   "https://01t71ck4-3000.inc1.devtunnels.ms",
